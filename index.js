e2652f0644979b026eb3ae3d0c0e95436a56c60f
Harish Kunwar <kunwarharish11@gmail.com>
2023-07-22 20:19:26 +0545

    feat: Initialize project with MVC structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require("express");
+
+require("dotenv").config();
+
+const app=express();
+
+
+
+const port=process.env.PORT||8080
+app.listen(port,()=>{
+    console.log(`server running at port:${port}`);
+})
