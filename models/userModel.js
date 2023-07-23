61519d25a359a7a93efaee94d0875e20295fd61c
Harish Kunwar <kunwarharish11@gmail.com>
2023-07-23 14:08:03 +0545

    fix: Implement pre save hook for password hashing using bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose")
+const bcrypt=require("bcrypt")
 
 const userSchema=new mongoose.Schema({
     name:{
@@ -23,6 +24,15 @@ const userSchema=new mongoose.Schema({
             type:Boolean,
             default:false
         },
-},{timestamps:true}
+},
+{timestamps:true}
 );
+
+userSchema.pre('save',async function (next){
+    const salt=await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password,salt)
+    next();
+})
+
+
 module.exports=mongoose.model("User",userSchema)
