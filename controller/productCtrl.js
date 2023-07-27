44ecbb2c92121eb62f6b04b73a7016f9807cf3bb
Harish Kunwar <kunwarharish11@gmail.com>
2023-07-27 14:49:57 +0545

    feat: Implement single product retrival

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -10,7 +10,28 @@ const createProduct=asyncHandler(async (req,res)=>{
     const newProduct=await Product.create(req.body);
     res.status(200).json(newProduct);
 
+});
+
+//get a single product
+const getProduct=asyncHandler(async (req,res)=>{
+    const {id}=req.params;
+    const product=await Product.findById(id);
+    if(product){
+        res.status(200).json({
+            title:product.title,
+            description:product.description,
+            price:product.price,
+            category:product.category,
+            brand:product.category,
+            images:product.images
+        })
+    }
+    else{
+        res.status(404).json({message:"product not found"});
+    }
 })
 
 
-module.exports={createProduct};
+module.exports={
+    createProduct,getProduct
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const { createProduct } = require("../controller/productCtrl");
+const { createProduct, getProduct } = require("../controller/productCtrl");
 const {authMiddleware,isAdmin}=require("../middleware/authMiddleware")
 
 const router=express.Router();
@@ -7,5 +7,8 @@ const router=express.Router();
 //route to create new product
 router.post("/new-product",authMiddleware,isAdmin,createProduct)
 
+
+//route to get single product
+router.get('/:id',getProduct)
 
 module.exports=router;
