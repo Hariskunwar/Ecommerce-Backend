97c8e4abc79bab01ef98a614aa9d93915f9869b3
Harish Kunwar <kunwarharish11@gmail.com>
2023-07-27 19:18:20 +0545

    feat: Implement pagination functionlity

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -56,6 +56,16 @@ const getAllProduct=asyncHandler(async (req,res)=>{
         query.select("-__v")
     }
 
+    //pagination
+    const page=req.query.page;
+    const limit=req.query.limit;
+    const skip=(page-1)*limit;
+    query=query.skip(skip).limit(limit)
+    if(req.query.page){
+        const productCount=await Product.countDocuments();
+        if(skip>=productCount) throw new Error("this page does not exist");
+    }
+
     const products=await query;
     res.status(200).json(products);
 });
